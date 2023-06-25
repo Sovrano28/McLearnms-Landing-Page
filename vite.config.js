97f646fa9f95cd66b4a152f9937be40ca7b3d028
Ayokanmi Adeyemi <101461930+Sovrano28@users.noreply.github.com>
2023-06-25 23:58:11 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 const path = require('path')
+import { defineConfig } from 'vite';
 
-export default {
+export default defineConfig({
   root: path.resolve(__dirname, 'src'),
   build: {
-    outDir: '../dist'
+    outDir: '../dist',
+    base: '/McLearnms-Landing-Page/',
   },
   server: {
     port: 8080
   }
-}
+});
