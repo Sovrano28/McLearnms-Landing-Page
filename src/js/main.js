95a31118b4355212defe6ed073bd9691bdf9b446
Ayokanmi Adeyemi <101461930+Sovrano28@users.noreply.github.com>
2023-07-04 03:43:58 +0100

    finished integrating swiper.js for the testimonial cards swiping functionalities

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,22 @@
 import '../scss/style.scss';
 import * as bootstrap from 'bootstrap';
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "auto",
+  // slidesPerView: 1,
+  centeredSlides: true,
+  spaceBetween: 30,
+  // loop: true,
+  // autoplay: {
+  //   delay: 5000,
+  //   disableOnInteraction: true,
+  // },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
